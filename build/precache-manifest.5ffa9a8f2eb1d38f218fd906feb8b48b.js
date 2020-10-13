34d76b596bd34e3e6789f94b468067bb987593b8
Gaurav Chauhan <25347973+gaurav-210@users.noreply.github.com>
2020-10-13 15:05:21 +0530

    production build

---
+++ build/precache-manifest.5ffa9a8f2eb1d38f218fd906feb8b48b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80edd9fb174166c2817d9f9fcf26baaf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce3bd1c67f397a0e67c4",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "645b72da1e83c6c6846f",
+    "url": "/static/css/main.113080c1.chunk.css"
+  },
+  {
+    "revision": "ce3bd1c67f397a0e67c4",
+    "url": "/static/js/2.7297db1a.chunk.js"
+  },
+  {
+    "revision": "acb364211461e18f66aec1e8c68e542c",
+    "url": "/static/js/2.7297db1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "645b72da1e83c6c6846f",
+    "url": "/static/js/main.02005c07.chunk.js"
+  },
+  {
+    "revision": "3a2383183f24fb4a8cce",
+    "url": "/static/js/runtime-main.008ef387.js"
+  }
+]);
